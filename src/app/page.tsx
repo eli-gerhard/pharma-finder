42ddfa9acd97a11605efc7a97ed3cb721ab14e07
Eli Gerhard <eli4@gerhard.org>
2025-02-16 14:34:08 -0500

    Update background design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import Link from 'next/link'
 import Header from '@/components/ui/Header'
 import { useState, useEffect } from 'react'
+import { TextSearch, PillBottle, Users } from 'lucide-react';
 
 export default function Home() {
   const [isMobile, setIsMobile] = useState(false)
@@ -26,13 +27,13 @@ export default function Home() {
   return (
     <>
     <Header />
-    <main className="p-4 flex justify-center mt-16">
+    <main className="p-4 flex flex-col justify-center mt-16">
       <div className={`max-w-7xl ${isMobile ? 'flex-col' : 'flex'} items-center`}>
-        <div className={`${isMobile ? 'w-full text-center' : 'w-1/2'} px-4`}>
+        <div className={`${isMobile ? 'w-full text-center border-b border-[var(--puborder)] pb-8 pt-8' : 'w-1/2'} px-4`}>
           <div className="text-5xl px-4">
             Helping you navigate medication shortages
           </div>
-          <div className="text-sm px-4 mt-4">
+          <div className="text-sm px-4 mt-8">
             Crowdsourcing which pharmacies have your medication in stock
           </div>
         </div>
@@ -56,6 +57,26 @@ export default function Home() {
           >
             Find Nearby Medication
           </Link>
+        </div>
+      </div>
+      <div className="text-3xl px-4 text-center mt-48 mb-8">
+        How it works
+      </div>
+      <div className={`max-w-7xl ${isMobile ? 'flex-col' : 'flex'} items-start`}>
+        <div className={`${isMobile ? 'w-full border-b border-[var(--puborder)] pb-8 pt-8' : 'w-1/2'} flex flex-col items-center text-center px-4`}>
+          <TextSearch size={32} color="var(--accent)" className="mb-2" />
+          <span className="text-lg text-center mb-2 text-[var(--accent)]">Faster Searching</span>
+          <span className="text-sm text-center">InStockMed shows you where your medication was filled recently, saving you search time</span>
+        </div>
+        <div className={`${isMobile ? 'w-full border-b border-[var(--puborder)] pb-8 pt-8' : 'w-1/2'} flex flex-col items-center text-center px-4`}>
+          <PillBottle size={32} color="green" className="mb-2" />
+          <span className="text-lg text-center mb-2 text-green-600">Fill The Prescription</span>
+          <span className="text-sm text-center">Confirm the stock with the pharmacy, and fill your prescription</span>
+        </div>
+        <div className={`${isMobile ? 'w-full border-b border-[var(--puborder)] pb-8 pt-8' : 'w-1/2'} flex flex-col items-center text-center px-4`}>
+          <Users size={32} color="var(--accent)" className="mb-2" />
+          <span className="text-lg text-center mb-2 text-[var(--accent)]">Crowd Sourced Info</span>
+          <span className="text-sm text-center">After your prescription is filled, enter the details on InStockMed to help others</span>
         </div>
       </div>
     </main>
